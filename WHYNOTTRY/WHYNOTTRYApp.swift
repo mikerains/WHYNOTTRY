d355f68cd5b475e69cfab1772212dbe64b918720
Mike Rains <iowamiker@hotmail.com>
2023-12-03 18:51:03 -0800

    Initial Commit

---
+++ WHYNOTTRY/WHYNOTTRYApp.swift
@@ -0,0 +1,17 @@
+//
+//  WHYNOTTRYApp.swift
+//  WHYNOTTRY
+//
+//  Created by Mike on 12/2/23.
+//
+
+import SwiftUI
+
+@main
+struct WHYNOTTRYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
